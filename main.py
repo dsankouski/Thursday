920816b7ce9e3a260d8f17508f8f26ce5e241da6
LutzBY <lutzby@gmail.com>
2025-02-27 13:18:13 +0300

    modified:   main.py Убран комментарий в строке 98

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def play_dice(chat_id, users):
         user_id = user.id
         user_name = user.first_name
         bot.send_message(chat_id=chat_id,
-                         text="{}:".format(user_name), # я с маркдаун не сталкивался ранее я хз надо ли тут оставлять []
+                         text="{}:".format(user_name),
                          parse_mode='MarkdownV2')
         dice = bot.send_dice(chat_id=chat_id, emoji="🎲").dice
 
